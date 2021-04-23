141ed3f1f696e2868eaccda505f3770fbe675909
Veudzveulay <32245083+Veudzveulay@users.noreply.github.com>
2021-04-23 17:20:57 +0200

    PHP
    Il faut mettre le name dans $_POST

--- exo.php
+++ exo.php
@@ -1,25 +1,12 @@
 <?php
-$mail = $_POST["email"]; // récupere ce qui est dans l'input de email
+$name = $_POST["username"];
+$prenom = $_POST["usersurname"];
+$tel = $_POST["telephone"]; // récupere ce qui est dans l'input de email
 $age = $_POST["age"]; // récupere ce qui est dans l'input de age
-$nom = $_POST["code"];
 
-if (empty($age)) // si il n'y a rien dans age: fait echo 
-{
-    echo "La case age n'est pas rempli";
-}
-if (empty($mail)) // si il n'y a rien dans email: fait echo 
-{
-    echo "<br> La case mail n'est pas rempli";
-}
 
-if(preg_match ( " /^.+@.+\.[a-zA-Z]{2,}$/ " , $mail )) // si email possède tout les caractéristique d'un email correct
-{
-    if (!empty($age)) // si il y a quelque chose dans age: fait echo 
-    {
-    echo "<br>Votre adresse mail est valide et vous avez $age ans ";
-    }
-} 
-else // l'email n'a pas les caratéristique d'un email correct   
-{
-    echo"<br>L'email n'est pas valide";
-}
+echo "$name";
+echo "$prenom";
+echo "$tel";
+echo "$age";
+?>
