270d2662ce7605c911e22ab6290f9e95645f38f1
Veudzveulay <32245083+Veudzveulay@users.noreply.github.com>
2021-04-24 15:14:55 +0200

    php à finir avec filter_var

--- exo.php
+++ exo.php
@@ -1,12 +1,20 @@
-<?php
-$name = $_POST["username"];
-$prenom = $_POST["usersurname"];
-$tel = $_POST["telephone"]; // récupere ce qui est dans l'input de email
-$age = $_POST["age"]; // récupere ce qui est dans l'input de age
-
-
-echo "$name";
-echo "$prenom";
-echo "$tel";
-echo "$age";
-?>
+    <?php
+    $name = $_POST["username"];
+    $prenom = $_POST["usersurname"];
+    $tel = $_POST["telephone"]; // récupere ce qui est dans l'input de email
+    $age = $_POST["age"]; // récupere ce qui est dans l'input de age
+    $adresse = $_POST["adresse"]; // récupere ce qui est dans l'input de age
+    
+    if(preg_match("/^([a-zA-Z' ]+)$/",$name)) {
+        echo "Votre nom est correct";
+    }
+    else {
+        echo "Vous avez mal écrit votre nom";
+    }
+    if(preg_match("/^([a-zA-Z' ]+)$/",$prenom)) {
+        echo "<br>Votre prenom est correct";
+    }
+    else {
+        echo "Vous avez mal écrit votre prenom"; 
+    }
+    ?>
