e830752e20db2f1d9ec7de4c1869519d363a7025
DisintGreeD <78026837+DisintGreeD@users.noreply.github.com>
2021-04-26 11:09:06 +0200

    Php fini

--- validation.php
+++ validation.php
@@ -1,15 +1,35 @@
 <?php
-$name = $_POST["usename"]; // récupere ce qui est dans l'input de email
-$surname = $_POST["usersurname"]; // récupere ce qui est dans l'input de age
-$sexe = $_POST["sexe"];
-$age = $_POST["age"];
-$adresse = $_POST["adresse"];
+    $name = $_POST["username"];
+    $prenom = $_POST["usersurname"];
+    $tel = $_POST["telephone"];
+    $age = $_POST["age"];
+    $adresse = $_POST["adresse"];
+
+    if (preg_match("/^([a-zA-Z' ]+)$/", $name)) {
+        echo "Votre nom est correct";
+    } else {
+        echo "Vous avez mal écrit votre nom";
+    }
+    if (preg_match("/^([a-zA-Z' ]+)$/", $prenom)) {
+        echo "<br>Votre prenom est correct";
+    } else {
+        echo "<br>Vous avez mal écrit votre prenom";
+    }
+    if (filter_var($adresse, FILTER_VALIDATE_EMAIL)) {
+        echo ("<br> adresse mail valide");
+    } else {
+        echo ("<br> adresse mail invalide");
+    }
+    if (filter_var($age, FILTER_SANITIZE_NUMBER_INT)) {
+        echo ("<br> Vous avez $age");
+    } else {
+        echo ("<br> Veuillez renseigner un âge valide");
+    }
+    if (filter_var($tel, FILTER_SANITIZE_NUMBER_INT)) {
+        echo "<br>Votre numéro: $tel est bon";
+    } else {
+        echo "<br>Numéro de téléphone pas valide";
+    }
 
 
-if (filter_var($adresse, FILTER_VALIDATE_EMAIL)) {
-    echo("$adresse adresse mail valide");
-  } else {
-    echo("$adresse adresse mail invalide");
-  }
-
-?>
+    ?>
