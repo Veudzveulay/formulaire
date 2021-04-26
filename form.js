ed0473544bb83698e906527c674aee94fc2aa646
Veudzveulay <32245083+Veudzveulay@users.noreply.github.com>
2021-04-26 12:04:36 +0200

    JS avec les vérifs
    il manque la vérification pour l'âge.

--- form.js
+++ form.js
@@ -11,89 +11,107 @@ function response() { // suite de la fonction ONLOAD
   console.log(this.responseText); // pour voir le texte dans la console 
   document.getElementById("response").innerHTML = this.responseText; // permet de faire apparaitre le texte 
 }
-button.addEventListener("click", function () 
-{
-      if (document.getElementById("name").value == "") // si email n'est pas rempli exécute la suite 
-      { // met en rouge email 
-        var name = document.getElementById("name");
-        name.style.background = 'red';
+button.addEventListener("click", function () {
+  // Vérification du nom
+  if (validation_Nom_Prenom(document.getElementById("name").value)) 
+  
+  {  
+    var name = document.getElementById("name");
+    name.style.background = '';
+    var p = 1;
 
-      } else { // remet E-mail en noir
-        var name = document.getElementById("name");
-        name.style.background = '';
-        var p = 1;
-      }
-        if (document.getElementById("prenom").value == "") // si email n'est pas rempli exécute la suite 
-        { // met en rouge email 
-          var surname = document.getElementById("prenom");
-          surname.style.background = 'red';
+  } else { 
+    var name = document.getElementById("name");
+    name.style.background = 'red';
+  }
+  // Vérification du prénom
+  if (validation_Nom_Prenom(document.getElementById("prenom").value))
+  {  
+    var surname = document.getElementById("prenom");
+    surname.style.background = '';
+    var v = 1;
 
-        } else { // remet E-mail en noir
-          var surname = document.getElementById("prenom");
-          surname.style.background = '';
+  } else { 
+    var surname = document.getElementById("prenom");
+    surname.style.background = 'red';
 
-          var v = 1;
-        }
-        if (document.getElementById("age").value == "") // si age n'est pas rempli exécute la suite 
-        {
-          // met en rouge Votre age 
-          var age = document.getElementById("age");
-          age.style.background = 'red';
-
-
-
-        } else { // remet Votre age en noir
-          var age = document.getElementById("age");
-          age.style.background = '';
-          var y = 1;
-        }
-        if (document.getElementById("numero").value == "") // si age n'est pas rempli exécute la suite 
-        {
-          // met en rouge Votre age 
-          var numero = document.getElementById("numero");
-          numero.style.background = 'red';
+  }
+  // Vérification de l'âge
+  if (document.getElementById("age").value == "")  
+  {
+     
+    var age = document.getElementById("age");
+    age.style.background = 'red';
 
 
 
-        } else { // remet Votre age en noir
-          var numero = document.getElementById("numero");
-          numero.style.background = '';
-          var m = 1;
-        }if (document.getElementById("adresse").value == "") // si age n'est pas rempli exécute la suite 
-        {
-          // met en rouge Votre age 
-          var adresse = document.getElementById("adresse");
-          adresse.style.background = 'red';
+  } else { 
+    var age = document.getElementById("age");
+    age.style.background = '';
+    var y = 1;
+  }
+  // vérfication du téléphone 
+  if (Verifier_Numero_Telephone(document.getElementById("numero").value))  
+  {
+    var numero = document.getElementById("numero");
+    numero.style.background = '';
+    var m = 1;
+
+  } else { // remet Votre age en noir
+    var numero = document.getElementById("numero");
+    numero.style.background = 'red';
+  }
+  // Vérification de l'adresse mail
+  if (validerEmail(document.getElementById("adresse").value))  
+  {
+    var adresse = document.getElementById("adresse");
+    adresse.style.background = '';
+    var o = 1;
+
+  } else { 
+    var adresse = document.getElementById("adresse");
+    adresse.style.background = 'red';
+  }
+  // Vérification du statut
+  if (document.getElementById("statut").value == "choix")  
+  {
+     
+    var statut = document.getElementById("statut");
+    statut.style.color = 'red';
 
 
 
-        } else { // remet Votre age en noir
-          var adresse = document.getElementById("adresse");
-          adresse.style.background = '';
-          var o = 1;
-        }if (document.getElementById("statut").value == "choix") // si age n'est pas rempli exécute la suite 
-        {
-          // met en rouge Votre age 
-          var statut = document.getElementById("statut");
-          statut.style.color = 'red';
+  } else { 
+    var statut = document.getElementById("statut");
+    statut.style.color = '';
+    var n = 1;
+  }
 
 
+  if (y == 1 && v == 1 && p == 1 && m == 1 && o == 1 && n == 1) {
+    var form = document.getElementById('formulaire'); // on récupere tout les inputs du formulaire
+    var formData = new FormData(form);
+    ajax(formData); // on exécute la fonction ajax avec les données du formulaire 
 
-        } else { // remet Votre age en noir
-          var statut = document.getElementById("statut");
-          statut.style.color = '';
-          var n = 1;
-        }
-      
-        
-        if (y == 1 && v == 1 && p == 1 && m == 1 && o == 1 && n == 1) {
-          var form = document.getElementById('formulaire'); // on récupere tout les inputs du formulaire
-          var formData = new FormData(form);
-          ajax(formData); // on exécute la fonction ajax avec les données du formulaire 
+    console.log(form); // affiche dans la console 
 
-          console.log(form); // affiche dans la console 
-          
-        } else {
-          document.getElementById("response").innerHTML = "";
-        }
-      });
+  } else {
+    document.getElementById("response").innerHTML = "";
+  }
+});
+
+function validerEmail(email) {
+  var mail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return mail.test(email);
+}
+
+function Verifier_Numero_Telephone(num_tel) {
+
+  var tel = new RegExp(/^(01|02|03|04|05|06|08)[0-9]{8}/gi);
+  return tel.test(num_tel);
+}
+function validation_Nom_Prenom(verif) {
+
+ var nom_Prenom = new RegExp(/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u);
+ return nom_Prenom.test(verif);
+}
